45000c20028d5077d310f3681c0af992fba5d761
Dongbin Kim <=>
2017-11-02 11:22:15 +0900

    Refactor on OOP based

--- js/spinboxFactory.js
+++ js/spinboxFactory.js
@@ -1,83 +1,71 @@
-const spinboxBuildFuncs = (() => {
-	function buildNumberInput() {
-		const input = document.createElement("input");
-
-		input.type = "text";
-		input.value = 200;
-		input.className = "number-input";
-
-		return input;
-	}
-
-	function buildNumberUpButton() {
-		const button = document.createElement("button");
-		const icon = document.createElement("i");
-
-		button.className = "button spinbox-btn number-up-btn";
-		button.dataset.buttonType = "up";
-		icon.className = "fa fa-caret-up fa-3x";
-
-		button.appendChild(icon);
-
-		return button;
-	}
-
-	function buildNumberDownButton() {
-		const button = document.createElement("button");
-		const icon = document.createElement("i");
-
-		button.className = "button spinbox-btn number-down-btn";
-		button.dataset.buttonType = "down";
-		icon.className = "fa fa-caret-down fa-3x";
-
-		button.appendChild(icon);
-
-		return button;
-	}
-
-	function getWrapperAround(className) {
-		const wrapper = document.createElement("div");
-
-		wrapper.className = className;
-		return wrapper;
-	}
-
-	return {
-		buildNumberInput,
-		buildNumberUpButton,
-		buildNumberDownButton,
-		getWrapperAround
-	};
-})();
-
 const root = document.getElementById("root");
 const errorTextContainer = document.getElementById("errorText");
-/**
- * Constants
- */
-const MIN_VALUE = 100;
-const MAX_VALUE = 300;
-const HOLD_DELAY = 500; // millisecond
-const SPIN_INTERVAL = 100; // millisecond
 
-/**
- * Holding Flag variables
- */
-let holdStarter = null;
-let holdActive = false;
-let spinIntervalId = null;
-let activeButtonType = null;
+function getWrapperAround(className) {
+	const wrapper = document.createElement("div");
 
-function Spinbox() {
+	wrapper.className = className;
+	return wrapper;
+}
+
+function Spinbox({minValue = 100, maxValue = 300, holdDelay = 500, spinInterval = 100} = {}) {
+	const spinboxBuildFuncs = {
+		buildNumberInput: () => {
+			const input = document.createElement("input");
+
+			input.type = "text";
+			input.value = 200;
+			input.className = "number-input";
+
+			return input;
+		},
+
+		buildNumberUpButton: () => {
+			const button = document.createElement("button");
+			const icon = document.createElement("i");
+
+			button.className = "button spinbox-btn number-up-btn";
+			button.dataset.buttonType = "up";
+			icon.className = "fa fa-caret-up fa-3x";
+
+			button.appendChild(icon);
+
+			return button;
+		},
+
+		buildNumberDownButton: () => {
+			const button = document.createElement("button");
+			const icon = document.createElement("i");
+
+			button.className = "button spinbox-btn number-down-btn";
+			button.dataset.buttonType = "down";
+			icon.className = "fa fa-caret-down fa-3x";
+
+			button.appendChild(icon);
+
+			return button;
+		}
+	};
+
 	this.numberInput = spinboxBuildFuncs.buildNumberInput();
 	this.numberUpButton = spinboxBuildFuncs.buildNumberUpButton();
 	this.numberDownButton = spinboxBuildFuncs.buildNumberDownButton();
+
+	this.getMinValue = () => minValue;
+	this.getMaxValue = () => maxValue;
+	this.getHoldDelay = () => holdDelay;
+	this.getSpinInterval = () => spinInterval;
+
+	this.holdStarter = null;
+	this.holdActive = false;
+	this.spinIntervalId = null;
+	this.activeButtonType = null;
 }
 
 Spinbox.prototype.init = function() {
-	const inputWrapper = spinboxBuildFuncs.getWrapperAround("spinbox-input-wrapper");
-	const buttonWrapper = spinboxBuildFuncs.getWrapperAround("spinbox-btn-wrapper");
-	const spinboxWrapper = spinboxBuildFuncs.getWrapperAround("spinbox-wrapper");
+	const inputWrapper = getWrapperAround("spinbox-input-wrapper");
+	const buttonWrapper = getWrapperAround("spinbox-btn-wrapper");
+	const spinboxWrapper = getWrapperAround("spinbox-wrapper");
 
 	inputWrapper.appendChild(this.numberInput);
 	buttonWrapper.appendChild(this.numberUpButton);
@@ -93,7 +81,7 @@ Spinbox.prototype.init = function() {
 
 Spinbox.prototype.addListeners = function() {
 	this.numberInput.addEventListener("blur", this.leaveNumberOnly);
-	this.numberInput.addEventListener("blur", this.setNumberIntoBoundary);
+	this.numberInput.addEventListener("blur", this.setNumberIntoBoundary.bind(this));
 	this.numberUpButton.addEventListener("mousedown", this.isHold.bind(this));
 	this.numberDownButton.addEventListener("mousedown", this.isHold.bind(this));
 	document.addEventListener("mouseup", this.stopSpinning.bind(this));
@@ -109,11 +97,13 @@ Spinbox.prototype.leaveNumberOnly = function(e) {
 Spinbox.prototype.setNumberIntoBoundary = function(e) {
 	const targetElement = e.target;
 	const number = Number(targetElement.value);
-	const isSmallerThanMin = number < MIN_VALUE;
-	const isBiggerThanMax = number > MAX_VALUE;
+	const minValue = this.getMinValue();
+	const maxValue = this.getMaxValue();
+	const isSmallerThanMin = number < minValue;
+	const isBiggerThanMax = number > maxValue;
 
 	if (isSmallerThanMin || isBiggerThanMax) {
-		targetElement.value = isSmallerThanMin ? MIN_VALUE : MAX_VALUE;
+		targetElement.value = isSmallerThanMin ? minValue : maxValue;
 	} else {
 		targetElement.value = number;
 	}
@@ -122,33 +112,33 @@ Spinbox.prototype.setNumberIntoBoundary = function(e) {
 Spinbox.prototype.isHold = function(e) {
 	const buttonType = e.currentTarget.dataset.buttonType;
 
-	activeButtonType = buttonType;
-	holdStarter = null;
-	holdStarter = setTimeout(() => {
+	this.holdStarter = null;
+	this.activeButtonType = buttonType;
+	this.holdStarter = setTimeout(() => {
 		// 계속 누르고 있는 상태.
 		this.keepSpinning(buttonType);
-		holdStarter = null;
-		holdActive = true;
-	}, HOLD_DELAY);
+		this.holdStarter = null;
+		this.holdActive = true;
+	}, this.getHoldDelay());
 };
 
 Spinbox.prototype.stopSpinning = function() {
-	const buttonType = activeButtonType;
+	const buttonType = this.activeButtonType;
 
-	if (holdStarter) {
-		// HOLD_DELAY보다 빨리 뗐을때(클릭)
-		clearTimeout(holdStarter);
+	if (this.holdStarter) {
+		// this.holdDelay보다 빨리 뗐을때(클릭)
+		clearTimeout(this.holdStarter);
 		this.spin(buttonType);
-	} else if (holdActive) {
-		clearInterval(spinIntervalId);
-		holdActive = false;
+	} else if (this.holdActive) {
+		clearInterval(this.spinIntervalId);
+		this.holdActive = false;
 	}
 };
 
 Spinbox.prototype.keepSpinning = function(buttonType) {
-	spinIntervalId = setInterval(() => {
+	this.spinIntervalId = setInterval(() => {
 		this.spin(buttonType);
-	}, SPIN_INTERVAL);
+	}, this.getSpinInterval());
 };
 
 Spinbox.prototype.spin = function(buttonType) {
@@ -157,10 +147,11 @@ Spinbox.prototype.spin = function(buttonType) {
 
 Spinbox.prototype.numberUp = function() {
 	try {
+		const maxValue = this.getMaxValue();
 		const inputValue = Number(this.numberInput.value);
 
-		if (inputValue === MAX_VALUE) {
-			throw new Error(`증가 최대치는 ${MAX_VALUE}입니다.`);
+		if (inputValue === maxValue) {
+			throw new Error(`증가 최대치는 ${maxValue}입니다.`);
 		} else {
 			errorTextContainer.textContent = "";
 			this.numberInput.value = inputValue + 1;
@@ -172,10 +163,11 @@ Spinbox.prototype.numberUp = function() {
 
 Spinbox.prototype.numberDown = function() {
 	try {
+		const minValue = this.getMinValue();
 		const inputValue = Number(this.numberInput.value);
 
-		if (inputValue === MIN_VALUE) {
-			throw new Error(`감소 최소치는 ${MIN_VALUE}입니다.`);
+		if (inputValue === minValue) {
+			throw new Error(`감소 최소치는 ${minValue}입니다.`);
 		} else {
 			errorTextContainer.textContent = "";
 			this.numberInput.value = inputValue - 1;
